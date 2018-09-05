95dd29553ac75917979a40c1e3f92e9560041d06
Sarah Weyand <sarahmackenzie015@gmail.com>
2018-09-05 16:55:01 -0400

    small semicolon changes

--- src/main/java/edu/gatech/oad/antlab/person/Person3.java
+++ src/main/java/edu/gatech/oad/antlab/person/Person3.java
@@ -43,8 +43,8 @@ public class Person3 {
 	 */
 	private String calc(String input) {
 	  String reverseName = "";
-	  for (int i = input.length() - 1, i > 0 , i--) {
-	  	reverseName += input[i];
+	  for (int i = input.length() - 1; i >=0; i--) {
+	  	reverseName += input.charAt(i);
 	  }
 	  return reverseName;
 	}
