900832a989d60aa0e9d81b048903ff980db12638
clairewcheng <claire.cheng@gmail.com>
2018-09-05 18:55:10 -0400

    Revert "please you know~"
    This reverts commit 226f4e78d68f8d9882febba243b82022ea75209a.

--- src/main/java/edu/gatech/oad/antlab/person/Person1.java
+++ src/main/java/edu/gatech/oad/antlab/person/Person1.java
@@ -37,7 +37,7 @@ public class Person1 {
 		} else {
 			String endString = input.substring(stringLength-2, stringLength-1);
 			String firstString = input.substring(0, stringLength-3);
-			input = endString + firstString;
+			input = firstString + endString;
 			return input;
 		}
 	}
