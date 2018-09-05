b7909af9b18fc26e581cb5b16d07c0e3cf949425
bthornburgh <bthornburgh3@gatech.edu>
2018-09-05 17:57:22 -0400

    Pushing new master with functioning Gradle file

--- src/main/java/edu/gatech/oad/antlab/pkg1/AntLabMain.java
+++ src/main/java/edu/gatech/oad/antlab/pkg1/AntLabMain.java
@@ -57,11 +57,11 @@ import edu.gatech.oad.antlab.pkg2.*;
           + ant22.getMessage() + ant31.getMessage();
 		  //Person1 replace P1 with your name
 		  //and gburdell1 with your gt id
-		  Person1 p1 = new Person1("P1");
+		  Person1 p1 = new Person1("Claire");
 		  toPrint += p1.toString("gburdell1");
 		  //Person2 replace P2 with your name
 		  //and gburdell with your gt id
-		  Person2 p2 = new Person2("Bryn");
+		  Person2 p2 = new Person2("Bryn ");
 		  toPrint += p2.toString("bthornburgh3");
 		  //Person3 replace P3 with your name
 		  //and gburdell3 with your gt id
@@ -73,7 +73,7 @@ import edu.gatech.oad.antlab.pkg2.*;
                   toPrint += p4.toString("chunter41");
                   //Person5 replace P4 with your name
                   //and gburdell5 with your gt id
-                  Person5 p5 = new Person5("P5");
+                  Person5 p5 = new Person5("Crystal");
                   toPrint += p5.toString("gburdell5");
 		  
         System.out.println(toPrint);
